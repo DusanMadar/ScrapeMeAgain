a2d59651dcab174dd7201b6f890d010d065e7f1b
Dusan Madar <madar.dusan@gmail.com>
2017-05-22 19:54:08 +0200

    Config - avoid KeyError when setting TOR_PASSWORD

--- config.py
+++ config.py
@@ -19,5 +19,5 @@ REQUEST_TIMEOUT = 5
 LOCAL_HTTP_PROXY = '127.0.0.1:8118'
 NEW_IP_MAX_ATTEMPTS = 1000
 REUSE_THRESHOLD = 10
-TOR_PASSWORD = os.environ['TOR_PASSWORD'] or 'put your password here'
+TOR_PASSWORD = os.environ.get('TOR_PASSWORD', None) or 'put your password here'
 TOR_PORT = 9051
