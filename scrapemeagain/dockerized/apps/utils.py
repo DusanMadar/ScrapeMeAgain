8b3b09cb093818381b4b62e39f70622f5895af9c
Dusan Madar <madar.dusan@gmail.com>
2018-02-03 15:03:14 +0100

    Dockerized: setup apps logging in app_factory()

--- scrapemeagain/dockerized/apps/utils.py
+++ scrapemeagain/dockerized/apps/utils.py
@@ -1,11 +1,13 @@
 from flask import Flask
 
+from scrapemeagain.utils.logger import setup_logging
 from scrapemeagain.dockerized.utils import apply_scraper_config
 
 
 def app_factory(name):
     """
-    Create a Flask app with the updated config and a basic `health` endpoint.
+    Create a Flask app using scraper specific config. Setup loging and add a
+    basic `health` endpoint.
 
     To be able to use scraper specific config in a module this function has to
     be called first, e.g.
@@ -21,6 +23,9 @@ def app_factory(name):
     apply_scraper_config()
 
     app = Flask(name)
+
+    logger_name = app.root_path.rsplit('/', 1)[-1]
+    setup_logging(logger_name)
 
     @app.route('/health/')
     def healthcheck():
