85288675e292e1fca0449ed31e460984751a9764
Dušan Maďar <madar.dusan@gmail.com>
2018-11-04 17:14:35 +0100

    Fix examplesite listing

--- scrapemeagain/scrapers/examplescraper/examplesite/app.py
+++ scrapemeagain/scrapers/examplescraper/examplesite/app.py
@@ -1,3 +1,5 @@
+import sys
+
 from flask import Flask, render_template, request
 
 
@@ -6,15 +8,15 @@ app = Flask(__name__)
 
 @app.route("/posts/")
 def post_list():
-    page = int(request.args.get("page", 0))
+    page = int(request.args.get("page", 1))
     page_prev = page - 1
     page_next = page + 1
     page *= 10
 
     return render_template(
         "list.html",
-        start=0 + page,
-        stop=10 + page,
+        start=page - 9,
+        stop=page,
         page_next=page_next,
         page_prev=page_prev,
     )
@@ -26,6 +28,9 @@ def post_detail(id):
 
 
 if __name__ == "__main__":
-    # TODO the IP below is for my local docker bridge. Add a function which
-    # can get it dynamically.
-    app.run(host="172.17.0.1", port=9090)
+    try:
+        host = sys.argv[1]
+    except IndexError:
+        host = "localhost"
+
+    app.run(host=host, port=9090)
